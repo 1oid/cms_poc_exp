569d0aa885cfb92c97645ec3d247206a474cea84
loid <1398231927@qq.com>
2022-03-01 00:50:30 +0800

    新增-s参数指定某一个脚本

--- scan.py
+++ scan.py
@@ -25,14 +25,18 @@ class Runner(object):
         Output.log_info("loaded {} plugins for {}".format(len(scripts), path))
         return scripts
 
-    def check(self, url, cms):
+    def check(self, url, cms, script=None):
         self._check_cms(cms)
+        Module.set_path(os.path.join(Module.get_abs_path(), "exploits", cms))
 
         scripts = self.init_exploit_scripts(cms)
+        Output.log_info("checking {}".format(url))
+
+        if script:
+            scripts = [script]
 
         for script in scripts:
-            Module.set_path(os.path.join(Module.get_abs_path(), "exploits", cms))
-            script = script[:-3]
+            script = script[:-3] if script[-3:] == ".py" else script
             module = Module.import_mod(script)
 
             try:
@@ -43,12 +47,12 @@ class Runner(object):
             except Exception as e:
                 Output.log_error(str(e))
 
-    def run(self, urls, module, is_all=False):
+    def run(self, urls, module, is_all=False, script=None):
         urls = [urls] if isinstance(urls, str) else urls
         modules = [module] if not is_all else self.folders
 
         for module in modules:
-            [self.check(url, module) for url in urls]
+            [self.check(url, module, script) for url in urls]
 
 
 if __name__ == '__main__':
@@ -59,7 +63,8 @@ if __name__ == '__main__':
     parser.add_option("-u", "--url", dest="url", help="URL链接", default=None)
     parser.add_option("-f", "--urls", dest="urls", help="urls文件", default=None)
     parser.add_option("-m", "--module", dest='module', help="cms模块名", default=None)
-    parser.add_option("-a", "--all", dest='all', action="store_true", help="All Cms Plugin Load")
+    parser.add_option("-s", "--script", dest='script', help="指定某个插件, 前提指定--module参数", default=None)
+    parser.add_option("-a", "--all", dest='all', action="store_true", help="指定--all加载所有的poc")
     options, args = parser.parse_args()
 
     # Runner().check(options, cms)
@@ -69,6 +74,6 @@ if __name__ == '__main__':
     runner = Runner()
     if not options.url and options.urls is not None:
         with open(options.urls) as f:
-            runner.run([url.strip() for url in f.readlines()], options.module, is_all=options.all)
+            runner.run([url.strip() for url in f.readlines()], options.module, is_all=options.all, script=options.script)
     else:
-        runner.run(options.url, options.module, is_all=options.all)
+        runner.run(options.url, options.module, is_all=options.all, script=options.script)
